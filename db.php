d24c4a9321740b21ba49960d326e5e11719a8ff1
William Au <iblogger@gmail.com>
2022-08-29 16:06:56 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,13 @@
 <?php
+
+
+$url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host   = $cleardb_url["host"];
+$user = $cleardb_url["user"];
+$pass = $cleardb_url["pass"];
+$dbname     = substr($cleardb_url["path"],1);
+
 	$database_username = 'root';
 	$database_password = '';
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_samples', $database_username, $database_password );
+	$pdo_conn = new PDO( 'mysql:host='.$host.';dbname='.$dbname, $user, $pass );
 ?>
